6a2f63c4dbde68f147e88e51939f18dd789e95d3
Atsuto Ishinaga <atsutoishinaga@isb2-dhcp-170-212.naist.jp>
2020-04-29 19:04:51 +0900

    detect_turnに-debug,-rangeオプションを追加

--- main.py
+++ main.py
@@ -14,13 +14,15 @@ def main(command_line=None):
         help='Print debug info'
     )
     subprasers = parser.add_subparsers(dest='command')
-
-    show_route = subprasers.add_parser('show_route', help='show route people')
-    show_route.add_argument('file_path', help='file_path to show_route')
-
-    detect_turn = subprasers.add_parser('detect_turn', help='detect_turn')
-    detect_turn.add_argument('file_path', help='file_path to detect_turn')
-    #引数を解析する
+#show_route
+    show_route = subprasers.add_parser('show_route', help='show route from GPS data')
+    show_route.add_argument('file_path', help='file_path for show_route')
+#detect_turn
+    detect_turn = subprasers.add_parser('detect_turn', help='detect_turn from only GPS data')
+    detect_turn.add_argument('file_path', help='file_path for detect_turn')
+    detect_turn.add_argument('-d','--debug' , action='store_true')
+    detect_turn.add_argument('-r','--range' ,type = int, nargs = 2 ,help='select range [start] [end]')
+#引数を解析する
     args = parser.parse_args(command_line)
     if args.debug:
         print("debug: " + str(args))
